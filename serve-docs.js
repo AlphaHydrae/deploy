53d660499af1b1a09a000b4f8e0d87dd772e432a
Simon Oulevay <simon.oulevay@gmail.com>
2017-06-30 21:32:07 +0200

    Docco documentation

---
+++ serve-docs.js
@@ -0,0 +1,26 @@
+const _ = require('lodash');
+
+var env = {
+  port: process.env.PORT,
+  host: process.env.HOST,
+  browser: process.env.BROWSER
+};
+
+var config = {};
+try {
+  config = require('./config');
+} catch(e) {
+  // Ignore missing or invalid config file
+}
+
+var defaults = {
+  port: 4000,
+  host: '127.0.0.1'
+};
+
+var fixed = {
+  root: './docs',
+  wait: 250
+};
+
+require('live-server').start(_.defaults(env, config, defaults, fixed));
