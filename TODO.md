b3687a508c1a8ea4cf52ea3129c61bd53d418644
Simon Oulevay <simon.oulevay@gmail.com>
2017-07-01 20:29:37 +0200

    Finish documentation

--- TODO.md
+++ TODO.md
@@ -1,6 +1,15 @@
 # TODO
 
-* make sure `get_all_config_values` does not return a blank line when no value is found
-* use environment variable last when getting multiple values
-* Verbosity (simple commands, full commands)
+* show h3 in toc
+* do not run pre-deploy hooks for first deployment
+* add command line arguments for ssh options and others
+* fix inheritance (no duplicates)
+* add confirmation prompts
+* verbosity (simple commands, full commands)
 * npm-like Scripts
+* make configuration file optional
+* support a symlink for the configuration file
+* create a deployment log (include commit)
+* make sure `get_all_config_values` does not return a blank line when no value is found
+* add commands to manipulate previous releases (print info, rollback)
+* add config-section command
--- res/docs.js
+++ res/docs.js
@@ -1,5 +1,25 @@
 $(function() {
+  // Make all external links open a new tab/window
+  $('a').not('[href^="#"]').attr('target', '_blank');
+
+  // Mark docco sections with a title for styling
   $('ul.sections li').not('#title').filter(function() {
-    return $(this).find('.annotation h2, .annotation h3').length;
-  }).addClass('title');
+    return $(this).find('.annotation h2').length;
+  }).addClass('title title-2').end().filter(function() {
+    return $(this).find('.annotation h3').length;
+  }).addClass('title title-3');
+
+  // Build a table of contents
+  var toc = $('#toc');
+  var tocSection = toc.closest('li');
+
+  // Only include level 2 titles by default
+  var mainTitles = tocSection.nextAll('li').find('h2');
+  mainTitles.each(function() {
+    var title = $(this);
+    toc.append($('<li />').html($('<a />').attr('href', '#' + title.attr('id')).html(title.html())));
+  });
+
+  // Show the table of contents (it is hidden while not built)
+  toc.css('display', 'block');
 });
