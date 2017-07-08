63db68575d8be2a7c93a7c8fa8b2b988f66f2de0
Simon Oulevay <simon.oulevay@gmail.com>
2017-07-08 16:20:14 +0200

    Add version & license badges

--- update-version.js
+++ update-version.js
@@ -9,7 +9,13 @@ if (!script.match(versionRegexp)) {
   return console.warn(chalk.red(`Cannot find version line in bin/deploy\n`));
 }
 
+const versionBadgeRegexp = /^(\#\s*\[\!\[npm version\]\([^\)]+)\d+\.\d+\.\d+([^\)]+\)\]\([^\)]+\))$/m;
+if (!script.match(versionBadgeRegexp)) {
+  return console.warn(chalk.red(`Cannot find version badge line in bin/deploy\n`));
+}
+
 script = script.replace(versionRegexp, `VERSION=${version}`);
+script = script.replace(versionBadgeRegexp, `$1${version}$2`)
 fs.writeFileSync('bin/deploy', script, { encoding: 'utf8' });
 
 console.log(chalk.green(`Version updated to ${version} in bin/deploy`));
